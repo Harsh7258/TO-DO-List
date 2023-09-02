5a3548bb9678d21a0d62f71555f7485cfb0d95d3
K Harsh <harshnaidu2u@gmail.com>
2023-09-03 00:16:44 +0530

    TODOS logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,43 @@ import "./styles.css";
 
 export default function App() {
   const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e){
+    e.preventDefault();
+    // prevents refreshing the page every time
+
+    setTodos((currTodos) => {
+      return[
+        ...currTodos,
+        {id: crypto.randomUUID(), title: newItem, completed: false}
+      ];
+    });
+
+    setNewItem("");
+  };
+
+  function toggleTodo(id, completed){
+    setTodos((currTodos) => {
+      return currTodos.map((toDo) => {
+        if(toDo.id === id){
+          return {...toDo, completed}
+        }
+      });
+    });
+  };
+
+  function deleteTodo(id){
+    setTodos((currTodos) => {
+      return currTodos.filter((todo) => todo.id !== id)
+    });
+  };
 
   return (
     <> 
-    <form className="new-item-form">
+    <form
+    onSubmit={handleSubmit}
+     className="new-item-form">
       <div className="form-row">
         <label htmlFor="item">New Item</label>
         <input 
@@ -16,23 +49,30 @@ export default function App() {
         <button className="btn">Add</button>
       </div>
     </form>
+
     <h1 className="header">TO-DO List</h1>
+
     <ul className="list">
-      <li>
-        <label>
-          <input type="checkbox" />
-          Item 1
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
-      <li>
-        <label>
-          <input type="checkbox" />
-            Item 2
+      {todos.length === 0 && "NO TO-DOS"}
+      {todos.map((todo) => {
+        return(
+          <li key={todo.id}>
+          <label>
+            <input 
+            type="checkbox" 
+            checked={todo.completed}
+            onChange={(e) => toggleTodo(todo.id, e.target.checked)}/>
+            {todo.title}
           </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+          <button 
+          onClick={() => deleteTodo(todo.id)}
+          className="btn btn-danger">Delete</button>
+          {/* deteleTodo is after a calback function beacuse without callback it will automatically delete the todos without get checked */}
+
+          {/* () => --> callback function is used to get onclick then it will call the deleteTodo without callback it will delete the todos binna click kiye */}
+      </li>
+        )})}
     </ul>
     </>
-  )
-}
+  );
+};
